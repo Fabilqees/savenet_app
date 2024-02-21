a3ec521c6571db8e02cee887bfc1a7456a39dac2
Fabilqees <bilqeesonize@gmail.com>
2024-02-21 09:50:06 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/qees/savings/savenet_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qees.savings.savenet_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/account_view.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AccountView extends StatelessWidget {
+  const AccountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Accounts");
+  }
+}
